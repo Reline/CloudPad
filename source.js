95bb446daddffe20762083c82fa5ab7c8c0dddd2
Reline <nathanielreline@gmail.com>
2016-06-10 19:43:22 -0400

    Implemented TogetherJS

--- source.js
+++ source.js
@@ -1,3 +1,18 @@
+var TogetherJSConfig_siteName = "CloudPad",
+TogetherJSConfig_toolName = "Collaboration",
+TogetherJSConfig_autoStart = true,
+TogetherJSConfig_dontShowClicks = true,
+TogetherJSConfig_suppressJoinConfirmation = true,
+TogetherJSConfig_suppressInvite = true,
+TogetherJSConfig_disableWebRTC = true,
+TogetherJSConfig_ignoreMessages = true,
+TogetherJSConfig_sharedContent = "";
+
 $(document).ready(function() {
-	console.log("Welcome to CloudPad!");
+    TogetherJSConfig_sharedContent = document.getElementById('sharedContent').innerText;
+    console.log(TogetherJSConfig_sharedContent);
+
+    $('#sharedContent').on('change keyup paste', function () {
+        TogetherJSConfig_sharedContent = $(this).val();
+    });
 });
